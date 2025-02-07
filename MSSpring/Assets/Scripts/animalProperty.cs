71d0a7f7df74e3a4c1aa71c0a10269b658bcc87e
Neilander <yw6472@nyu.edu>
2025-02-06 19:56:12 -0500

    SetupAnd some sprite

---
+++ MSSpring/Assets/Scripts/animalProperty.cs
@@ -0,0 +1,54 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewAnimalInfo", menuName = "AnimalProperty")]
+public class animalProperty : ScriptableObject
+{
+    public string animalName;
+    public Sprite animalCoreImg;
+
+    [Header("ForShop")]
+    public int animalPrice;
+
+    [Header("For Ball passing")]
+    public float baseYellowChange;
+    public float baseRedChange;
+    public float baseBlueChange;
+    public int restTurn;
+
+    [Header("For explain")]
+    /// <summary>
+    /// 得分动作的文本模板，例如 "得分 {0} 分，并额外获得 {1} 点奖励"
+    /// </summary>
+    public string scoreActionTemplate;
+    public int amount1;
+    public int amount2;
+    public int amount3;
+    
+    public string ballActionTemplate;
+    /// <summary>
+    /// 传球动作的文本模板，例如 "向{0}传球，进入{1}回合冷却"
+    /// </summary>
+    public int amount4;
+    public int amount5;
+    public int amount6;
+
+    /// <summary>
+    /// 生成得分动作的最终文本
+    /// </summary>
+    /// <returns>格式化后的得分描述字符串</returns>
+    public string returnScoreAction()
+    {
+        return string.Format(scoreActionTemplate, amount1,amount2,amount3);
+    }
+
+    /// <summary>
+    /// 生成传球动作的最终文本
+    /// </summary>
+    /// <returns>格式化后的传球描述字符串</returns>
+    public string returnBallAction()
+    {
+        return string.Format(ballActionTemplate, amount4, amount5, amount6);
+    }
+
+}
