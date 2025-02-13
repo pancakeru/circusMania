9b1db15adc7ceaf4ba30468f53112eeacd7ff1e9
Neilander <yw6472@nyu.edu>
2025-02-12 22:01:17 -0500

    build change

--- MSSpring/Assets/Scripts/iconAnimal.cs
+++ MSSpring/Assets/Scripts/iconAnimal.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using Unity.VisualScripting;
-using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
 
 public class iconAnimal : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
