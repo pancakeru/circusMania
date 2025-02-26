3a654cbf52e085852fc6545b46a7ef86e249a7ee
Zoe Chow <zoechowym22@gmail.com>
2025-02-26 16:28:50 -0500

    adding effect

--- MSSpring/Assets/Scripts/EndScreenScript.cs
+++ MSSpring/Assets/Scripts/EndScreenScript.cs
@@ -11,25 +11,62 @@ public class EndScreenScript : MonoBehaviour
     public TMP_Text totalScore;
     public TMP_Text moneyEarned;
 
+    private float starting = 0;
+    private float addSpeed = 1;
+
+    private enum DisplaySequence {
+        ShowScore,
+        Breakdown,
+        TotalCalc,
+        MoneyCalc,
+        EndButton
+    }
+    private DisplaySequence currentState;
 
     void Start()
     {
-        
-
+        currentState = DisplaySequence.ShowScore;
 
     }
 
     void Update()
     {
-        
+        switch (currentState) {
+            case DisplaySequence.ShowScore:
+
+                //1000 是分数字
+                DisplayText(scoreText, 1000, DisplaySequence.Breakdown);
+
+            break;
+
+            case DisplaySequence.Breakdown:
+
+            break;
+
+            case DisplaySequence.TotalCalc:
+                
+                DisplayText(totalScore, 10000, DisplaySequence.MoneyCalc);
+
+            break;
+
+            case DisplaySequence.MoneyCalc:
+
+                DisplayText(moneyEarned, 20000, DisplaySequence.EndButton);
+
+            break;
+        }
     }
 
 
-    void AddingEffect(float value) {
-        float starting = 0;
-
-        while (starting < value) {
-            starting++;
+    void DisplayText(TMP_Text text, float value, DisplaySequence nextState) {
+        if (starting < value) {
+            starting+= addSpeed;
+            addSpeed += 1;
+            text.text = starting.ToString();
+        } else {
+            starting = 0;
+            text.text = value.ToString();
+            currentState = nextState;
         }
     }
 
