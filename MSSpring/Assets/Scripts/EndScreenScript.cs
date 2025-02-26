be424db7840aa65b8659fd9da85dd6c48e80adbc
Zoe Chow <zoechowym22@gmail.com>
2025-02-26 17:20:36 -0500

    gooofy ah level text

--- MSSpring/Assets/Scripts/EndScreenScript.cs
+++ MSSpring/Assets/Scripts/EndScreenScript.cs
@@ -12,6 +12,7 @@ public class EndScreenScript : MonoBehaviour
     public TMP_Text scoreBreakdown;
     public TMP_Text totalScore;
     public TMP_Text moneyEarned;
+    public TMP_Text lvlName;
 
     private float starting = 0;
     private float addSpeed = 1;
@@ -37,13 +38,17 @@ public class EndScreenScript : MonoBehaviour
     void Update()
     {
         switch (currentState) {
-            case DisplaySequence.ShowScore:
 
-                //1000 是分数字
-                DisplayText(scoreText, 1000, DisplaySequence.Breakdown);
+            //第一部分-宏观信息
+            case DisplaySequence.ShowScore:
+                lvlName.text = "Level 1";
+                scoreText.text = "Required Score: 1000";
+
+                currentState = DisplaySequence.Breakdown;
 
             break;
 
+            //第二部分-分数结算
             case DisplaySequence.Breakdown:
                 lineDelay-= 1 * Time.deltaTime;
 
@@ -54,25 +59,30 @@ public class EndScreenScript : MonoBehaviour
                         currentState = DisplaySequence.TotalCalc;
                     }
                 } 
-
-
             break;
 
+            //第三部分收获
             case DisplaySequence.TotalCalc:
-                
+                //数量
                 DisplayText(totalScore, 10000, DisplaySequence.MoneyCalc);
 
             break;
 
             case DisplaySequence.MoneyCalc:
-
+                //金币
                 DisplayText(moneyEarned, 20000, DisplaySequence.EndButton);
+
+            break;
+
+            case DisplaySequence.EndButton:
+
+                //Play Again 按钮
 
             break;
         }
     }
 
-
+    
     void DisplayText(TMP_Text text, float value, DisplaySequence nextState) {
         if (starting < value) {
             starting+= addSpeed;
