e2f563250dd339e9a133d50577e2d5b9647ffe48
Zoe Chow <zoechowym22@gmail.com>
2025-03-05 11:37:31 -0500

    Audio manager setup

---
+++ MSSpring/Assets/Scripts/AudioManagerScript.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerScript : MonoBehaviour
+{
+    public AudioClip[] UI;
+    public AudioClip[] Environment;
+    public AudioClip[] Battle;
+    public static AudioManagerScript Instance;
+
+    public AudioSource environmentSource;
+    public AudioSource uiSource;
+    public AudioSource battleSource;
+
+    private bool isMuted = false;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayUISound(AudioClip clip)
+    {
+        if (!isMuted && clip != null)
+            uiSource.PlayOneShot(clip);
+    }
+
+    public void PlayBattleSound(AudioClip clip)
+    {
+        if (!isMuted && clip != null)
+            battleSource.PlayOneShot(clip);
+    }
+
+    public void PlayEnvironmentSound(AudioClip clip, bool loop = false)
+    {
+        if (!isMuted && clip != null)
+        {
+            environmentSource.clip = clip;
+            environmentSource.loop = loop;
+            environmentSource.Play();
+        }
+    }
+
+    public void ToggleMute()
+    {
+        isMuted = !isMuted;
+        environmentSource.mute = isMuted;
+        uiSource.mute = isMuted;
+        battleSource.mute = isMuted;
+    }
+
+    public void FadeOutEnvironment(float duration)
+    {
+        StartCoroutine(FadeOut(environmentSource, duration));
+    }
+
+    private System.Collections.IEnumerator FadeOut(AudioSource source, float duration)
+    {
+        float startVolume = source.volume;
+        while (source.volume > 0)
+        {
+            source.volume -= startVolume * Time.deltaTime / duration;
+            yield return null;
+        }
+        source.Stop();
+        source.volume = startVolume;
+    }
+}
