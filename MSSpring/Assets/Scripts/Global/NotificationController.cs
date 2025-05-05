e70407c873e23c406ed916a886ba16c117ad2963
yuya2358 <samlee2003@naver.com>
2025-05-05 13:43:35 -0400

    Ball Unlock

---
+++ MSSpring/Assets/Scripts/Global/NotificationController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.GraphicsBuffer;
+
+public class NotificationController : MonoBehaviour
+{
+    public Image ballImage;
+    RectTransform rectTransform;
+    Vector2 targetAnchoredPos = new Vector2 (510, 350);
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector2(960, 350);
+
+        StartCoroutine(UIAnimation());
+    }
+
+    IEnumerator UIAnimation()
+    {
+        float speed = 1000f;
+
+        while (rectTransform.anchoredPosition.x > targetAnchoredPos.x)
+        {
+            rectTransform.anchoredPosition = Vector2.MoveTowards(rectTransform.anchoredPosition, targetAnchoredPos, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        rectTransform.anchoredPosition = targetAnchoredPos;
+        Destroy(gameObject, 3f);
+    }
+}
