86c27bc54a4dfc952a1ce180238f6e82501574bc
Craneonz <onewaybreak@gmail.com>
2025-05-07 04:23:00 -0400

    SaveDataManager

---
+++ MSSpring/Assets/Scripts/Global/SaveData/SaveDataManager.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveDataManager : MonoBehaviour
+{
+    public static SaveDataManager Instance { get; private set; }
+
+    public GlobalSaveData globalSaveData;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than one SaveDataManager!" + transform + "-" + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    public void NewGame()
+    {
+        globalSaveData = new GlobalSaveData(new List<List<animalProperty>>(), 0, new List<Dictionary<string, int>>(), new AnimalBallPassTimes(), 0, 0, 0, new List<Dictionary<string, int>>(), new List<Dictionary<string, int>>(), new List<BallInfo>());
+    }
+
+    public void SaveGame(List<List<animalProperty>> animalPropertyListByLevel, int currentLevelIndex, List<Dictionary<string, int>> pointsByAnimal, AnimalBallPassTimes animalBallPassTimes, int currentCoin, int coinUsedForUpgrade, int maxBallPassTimes, List<Dictionary<string, int>> animalLevelList, List<Dictionary<string, int>> animalPriceList, List<BallInfo> ballInfoList)
+    {
+        globalSaveData = new GlobalSaveData(animalPropertyListByLevel, currentLevelIndex, pointsByAnimal, animalBallPassTimes, currentCoin, coinUsedForUpgrade, maxBallPassTimes, animalLevelList, animalPriceList, ballInfoList);
+    }
+
+    public void LoadGame()
+    {
+        if (globalSaveData == null)
+        {
+            Debug.Log("No save data found. Start a new game.");
+            NewGame();
+        }
+    }
+}
