32fdf1d6846f6614c30c67aa9771d10b26550ce4
yuya2358 <samlee2003@naver.com>
2025-05-08 18:18:49 -0400

    BallUnlocked

--- MSSpring/Assets/Scripts/AnimalTroupeScripts/TroupeController.cs
+++ MSSpring/Assets/Scripts/AnimalTroupeScripts/TroupeController.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -278,6 +279,13 @@ public class TroupeController : MonoBehaviour, ISaveData
         }
         else CanvasMain.instance.DisplayWarning("Already reached max level!");
 
+        //Check if achievementUnlocked
+        int achievementCounter = 0;
+        foreach (animalProperty tempAnimal in GlobalManager.instance.allAnimals.properies)
+        {
+            if (GlobalManager.instance.animalLevels[tempAnimal.animalName] == GlobalManager.instance.maxLevel) achievementCounter++;
+            if (achievementCounter == 7) MrShopManager.instance.AchievementUnlocked(6);
+        }
     }
 
     public int GetUpgradePrice(animalProperty property)
