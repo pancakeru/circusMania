50b78f87f65355940e460fad53cdc757f7816bb2
Craneonz <onewaybreak@gmail.com>
2025-05-08 23:04:07 -0400

    Start Screen Decide Panel

--- MSSpring/Assets/Scripts/Menu/StartScreenManager.cs
+++ MSSpring/Assets/Scripts/Menu/StartScreenManager.cs
@@ -4,6 +4,7 @@ public class StartScreenManager : MonoBehaviour
 {
     [SerializeField] Canvas canvas;
     [SerializeField] GameObject balloon;
+    [SerializeField] GameObject decidePanel;
 
     public void Enable()
     {
@@ -13,13 +14,26 @@ public class StartScreenManager : MonoBehaviour
 
     public void StartButton()
     {
-        GlobalManager.instance.NewGame();
-        canvas.enabled = false;
+        if (SaveDataManager.Instance.HasSaveDataExisted())
+        {
+            decidePanel.SetActive(true);
+        }
+        else
+        {
+            GlobalManager.instance.NewGame();
+            canvas.enabled = false;
+        }
     }
 
     public void LoadButton()
     {
         GlobalManager.instance.LoadGame();
+        canvas.enabled = false;
+    }
+
+    public void DecideConfirm()
+    {
+        GlobalManager.instance.NewGame();
         canvas.enabled = false;
     }
 
